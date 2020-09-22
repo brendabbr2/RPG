4dceb7886a56e4099294a34ad177c8dede243713
Brenda Badilla Rodríguez <brendabbr2@hotmail.com>
2020-09-22 12:24:40 -0600

    Subí Tienda.java
    Es la GUI de la tienda.

---
+++ Tienda.java
@@ -0,0 +1,27 @@
+package Tienda;
+
+import javax.swing.*;
+
+public class Tienda {
+    private JPanel Descripcion;
+    private JPanel Acciones;
+    private JButton Comprar;
+    private JButton Vender;
+    private JScrollPane Items;
+    private JLabel Dinero;
+    private JProgressBar vida;
+    private JProgressBar magia;
+    private JProgressBar ataque;
+    private JProgressBar defensa;
+    private JProgressBar energia;
+    private JLabel Vida;
+    private JLabel Magia;
+    private JLabel Ataque;
+    private JLabel Defensa;
+    private JLabel Energía;
+    private JTextField dinero;
+    private JTextField Descripcion_Objeto;
+    private JTextField Nombre_Objeto;
+    private JPanel Ganancia;
+    private JPanel Stats;
+}
